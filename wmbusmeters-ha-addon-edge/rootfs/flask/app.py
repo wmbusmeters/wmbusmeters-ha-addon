3e2100e45a7134cee1123b51115264ac4156e013
BIBOLV <49994376+BIBOLV@users.noreply.github.com>
2023-04-01 21:42:14 +0300

    Adding KEM file processing functionality
    Adding possibility to extract decryption key from KEM files within addon config WUI

--- wmbusmeters-ha-addon-edge/rootfs/flask/app.py
+++ wmbusmeters-ha-addon-edge/rootfs/flask/app.py
@@ -1,7 +1,11 @@
-import json, requests, os
+import json, requests, os, re, base64, zipfile, xmltodict
 from flask import Flask, jsonify, render_template, request
 from waitress import serve
 from threading import Thread
+from xml.dom import minidom
+from io import BytesIO
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
 
 app = Flask(__name__, static_url_path='')
 
@@ -12,6 +16,10 @@ URL_HEADER = { "Authorization": "Bearer " + os.environ.get('SUPERVISOR_TOKEN'), 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+	
+@app.route('/kem', methods=['GET', 'POST'])
+def kem():
+    return render_template('kem.html')
 
 @app.route('/save_json', methods=['POST'])
 def save_json_to_file():
@@ -40,6 +48,64 @@ def get_json():
             return jsonify(data)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+		
+@app.route('/decrypt', methods=['POST'])
+def decrypt():
+    file = request.files['file']
+    filename = request.files['file'].filename
+    password = request.form['password']
+    
+    kem_file_content = None
+
+    if (zipfile.is_zipfile(file)):
+        with zipfile.ZipFile(file,'r') as zipobj:
+            file_list = zipobj.namelist()
+            for file_name in file_list:
+                if file_name.endswith('.kem') or file_name.endswith('.kem2'):
+                    kem_file_content = zipobj.read(file_name)
+			
+            if (not kem_file_content):
+                return jsonify({"ERROR": "The zip file '%s' does not seem to contain any '.kem' file." % (filename)})
+    else:
+        file.seek(0)
+        kem_file_content = file.read()
+
+    try:
+        xmldoc = minidom.parseString(kem_file_content)
+        encrypedtext = xmldoc.getElementsByTagName('CipherValue')[0].firstChild.nodeValue
+        encrypeddata = base64.b64decode(encrypedtext)
+    except:
+        return jsonify({"ERROR": "The file '%s' does not seem to contain valid data - decryption failed." % (filename)})
+
+    data = BytesIO(file.read())
+
+    key = bytes(str(password).encode('utf-8'))
+    if (len(key) < 16): key += (16-len(key)) * b'\0'
+
+    backend = default_backend()
+    cipher = Cipher(algorithms.AES(key), modes.CBC(key), backend=backend)
+    decryptor = cipher.decryptor()
+    decryptedtext = decryptor.update(encrypeddata) + decryptor.finalize()
+
+    try:
+        decodedtext = decryptedtext.decode('utf-8')
+    except UnicodeDecodeError:
+        return jsonify({'ERROR': 'Looks like password is wrong - decryption failed!'})
+
+    trimmedText = re.sub(r'[\x00-\x08\x0b\x0c\x0e-\x1f\x7f-\xff]', '', decryptedtext.decode('utf-8'))
+    data_dict = xmltodict.parse(trimmedText)
+    json_data = json.dumps(data_dict)
+    resp = json.loads(json_data)
+    if 'MetersInOrder' in resp:
+        meterid = resp['MetersInOrder']['Meter']['MeterNo']
+        meterkey = resp['MetersInOrder']['Meter']['EncKeys']['DEK']
+        return jsonify({'OK': {'id': meterid, 'key': meterkey}})
+    elif 'Devices' in resp:
+        meterid = resp['Devices']['Device']['DeviceId']['SerialNumber']
+        meterkey = resp['Devices']['Device']['Keys']['Key']['Value']
+        return jsonify({'OK': {'id': meterid, 'key': meterkey}})	
+    else:
+        return jsonify({'ERROR': 'Unable to extract details from file'})
 
 if __name__ == '__main__':
     serve(app, host="127.0.0.1", port=5000)
